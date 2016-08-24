2f088bddb15d076c0e70e4386bb536d69a4d9f38
andrey <a.ashgaliev@gmail.com>
2016-08-24 14:13:36 +0500

    add input

--- Main.elm
+++ Main.elm
@@ -1,6 +1,7 @@
 import Html
 import Html.App as App
 import Html.Events exposing (..)
+import Html.Attributes exposing (value)
 
 main =  App.beginnerProgram {
     model  = init,
@@ -8,21 +9,22 @@ main =  App.beginnerProgram {
     update = update }
 
 init : Model
-init = { notes = [] } 
+init = { notes = [] , input_note = "" } 
 
-type Msg = AddNote | DeleteLast 
+type Msg = AddNote | ChangeInputNote String
 
-type alias Model = { notes : List String } 
+type alias Model = { notes : List String, input_note : String } 
 
 update : Msg -> Model -> Model
 update msg model =
   case msg of
-    AddNote -> { notes = "Some note" :: model.notes  }
-    _       -> { notes = model.notes  } 
+    AddNote           -> { model | notes = model.input_note :: model.notes, input_note = "" } 
+    ChangeInputNote a -> { model | input_note = a }
 
 view model = 
   Html.div [] [
      Html.ul [] (List.map (\x -> Html.li [] [Html.text x]) model.notes),
+     Html.input [value model.input_note, onInput (\x -> ChangeInputNote x) ] [],
      Html.button [ onClick AddNote ] [ Html.text "Add Note" ]  
   ]
 
